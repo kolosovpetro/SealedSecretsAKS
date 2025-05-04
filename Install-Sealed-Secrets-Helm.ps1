c44da743cd124d8f2bd17067e2374e17bc2d7ac6
kolosovpetro <kolosovp94@gmail.com>
2025-05-04 18:13:14 +0200

    encoding fix

--- Install-Sealed-Secrets-Helm.ps1
+++ Install-Sealed-Secrets-Helm.ps1
@@ -1,4 +1,4 @@
-﻿helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm repo update
 
 helm install sealed-secrets-controller sealed-secrets/sealed-secrets `
